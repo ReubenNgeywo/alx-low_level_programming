b3681102fe833f78bde5873f059c5bfc959bd526
ReubenNgeywo <ngeywruben@gmail.com>
2022-09-08 08:30:51 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #!/bin/bash
 #include <stdio.h> 
+
 /**
  * main -entry point
  *
- * Return: 0 after printing the function
+ * Return:(0) after printing the function
  */
 int main (void)
 {puts ("\"programming islike building a multilingual puzzle");
