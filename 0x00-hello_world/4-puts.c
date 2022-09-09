19d234f46cfaee1526a8e429131ab15d3a4cf40a
ReubenNgeywo <ngeywruben@gmail.com>
2022-09-09 01:15:39 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #!/bin/bash
-#include <stdio.h> 
+#Include <stdio.h> 
 
 /**
  * main -entry point
  *
- * Return:(0) after printing the function
+ * Return:(0)after printing the function
  */
-int main (void)
-{puts ("\"programming islike building a multilingual puzzle");
+int main(void)
+{
+	puts ("\"programming is like building a multilingual puzzle");
 	return (0);
 }
