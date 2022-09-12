6f824bf23f5cf3d7c92c046221814553885c91df
ReubenNgeywo <ngeywruben@gmail.com>
2022-09-11 17:18:10 -0700

    T6&4

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - print the string in the put function
  * Description: using the main function
- * this program prints "the size of various types on the computer
+ * this program prints "the sizes of various types on the computer
  * Return: 0
  */
 int main(void)
