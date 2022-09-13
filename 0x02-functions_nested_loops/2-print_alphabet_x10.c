f9ea0f98425e4072d395df8940b066ecd96ebb64
ReubenNgeywo <ngeywruben@gmail.com>
2022-09-13 11:26:35 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet in lower case,
+ * print_alphabet_x10 - prints 10 times the alphabet, in lower case,
  * followed by a new line
  */
 void print_alphabet_x10(void)
