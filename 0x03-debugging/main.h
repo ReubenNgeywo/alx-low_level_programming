ad1ccb0a1ce31b70d791d5d2851f7535578680f0
ReubenNgeywo <ngeywruben@gmail.com>
2022-09-15 02:22:03 -0700

    task 2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,6 +4,6 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest number(int, int, int);
+int largest_number(int, int, int);
 void print_remaining_days(int, int, int);
 int convert_day(int, int);
