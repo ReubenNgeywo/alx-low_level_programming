aeb886888ea6b2f284f1d0b920510fe3126b7371
ReubenNgeywo <ngeywruben@gmail.com>
2022-09-15 17:06:08 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * prints_numbers - numbers from 0 to 9
+ *
+ * Return always 0.
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
