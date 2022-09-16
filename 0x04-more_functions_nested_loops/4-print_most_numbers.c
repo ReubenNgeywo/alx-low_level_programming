349c157046d87dd719f8aa1e210de6953aaa07b6
ReubenNgeywo <ngeywruben@gmail.com>
2022-09-16 15:08:23 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,7 +4,7 @@
 /**
  * print_most_numbers - print numbers except 2 and 4
  *
- * Return: returns nothing 
+ * Return: returns nothing
  */
 
 void print_most_numbers(void)
@@ -18,6 +18,6 @@ void print_most_numbers(void)
 			continue;
 		}
 		putchar(n);
-	}
+		}
 	putchar(10);
 }
