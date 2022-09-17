6b9febeb90458c4c63ba296e653375055087c943
ReubenNgeywo <ngeywruben@gmail.com>
2022-09-16 18:26:59 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -17,7 +17,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void int main(void);
 void print_number(int n);
 
 #endif /* MAIN_H */
