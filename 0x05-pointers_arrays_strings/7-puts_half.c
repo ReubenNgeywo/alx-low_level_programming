9c9071b66a7a3c829943637f2eece598646d379b
ReubenNgeywo <ngeywruben@gmail.com>
2022-09-19 06:33:25 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,6 +14,7 @@ void puts_half(char *str)
 	len = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
+	{
 		len++;
 
 	n = (len / 2);
@@ -22,6 +23,7 @@ void puts_half(char *str)
 		n = ((len + 1) / 2);
 
 	for (i = n; str[i] != '\0'; i++)
-		_putchar(str[i];
-	_putchar('\n');
+		_putchar(str[i]);
 	}
+	putchar('\n);
+}
