b84bc43c03723ebaf32ea8722bd3b0308a3adce1
ReubenNgeywo <ngeywruben@gmail.com>
2022-09-20 01:00:33 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,9 +14,11 @@ int _atoi(char *s)
 	do {
 		if (*s == '-')
 			sign *= -1;
-			else if (*s >= '0' && *s <= '9')
-				num = num * 10 + (*s - '0');
-				else if (num > 0)
-					break;
+		else if (*s >= '0' && *s <= '9')
+			num = num * 10 + (*s - '0');
+		else if (num > 0)
+			break;
 	} while (*s++);
+	
+	return (num * sign);
 }
